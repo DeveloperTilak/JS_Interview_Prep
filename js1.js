4e1ce7ae8fa615fe8da9ddb41e89cfa65f104b84
Developer Awdhesh <awdheshthinker@gmail.com>
2023-05-13 23:51:56 +0530

    js1

---
+++ js1.js
@@ -0,0 +1,15 @@
+// 1. What is JavaScript and what are its key features?
+
+/*
+Defi: JS is high level langauge and used to build intractive web application.
+Ex: Van skelton can be HTML, Color and things where it can be placed that will be part of CSS
+and JS that's whole applying break, staring mobility and all functionality known as JS. 
+*/
+
+//2. What is the difference between undefined and null in JavaScript?
+/*
+    undefined: if a variable is declared but not initilized known as undefined.
+    Null: A variable has not been declared 
+    // let a =10;
+    console.log(a);
+    */
